0a61a6adc12b2782af0c14cfa98403e08986d8f5
Oscar Diaz <oads7@outlook.com>
2020-08-07 18:08:39 -0500

    Add Function to substract two integers, sub (a, b)

--- testGit.cpp
+++ testGit.cpp
@@ -2,18 +2,26 @@
 
 int multiply(const int& a, const inst& b){
   return a*b;
-}
 
 int add(const int& a, const int& b)
 {
  return a + b;
 } 
 
+int sub (const int& a, const int& b) {
+        return a-b;
+}
+
 int main()
 {
   std::cout << multiply(5,6) << std::endl;
   std::cout << "Hello W" << std::endl;
-  std::cout << add(5, 6) << std:: endl;
+  std::cout << add (5, 6) << std::endl;
+  
+  std::cout << "Sub (7-2): " << sub (7, 2) << std::endl;
+  
   return 0;
  
 }
+
+
