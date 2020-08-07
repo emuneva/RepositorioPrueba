83ff4f7ec24d84bb7474655c963795e3de00fa9c
Andres Vargas <vargasa@cua.edu>
2020-08-07 19:03:17 -0400

    Add fxn: multiply to take the product of two integers

--- testGit.cpp
+++ testGit.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 
+int multiply(const int& a, const inst& b){
+  return a*b;
+}
+
 int add(const int& a, const int& b)
 {
  return a + b;
 } 
 
-
 int main()
 {
+  std::cout << multiply(5,6) << std::endl;
   std::cout << "Hello W" << std::endl;
   std::cout << add(5, 6) << std:: endl;
   return 0;
