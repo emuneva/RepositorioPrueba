be995b0982fcb4e5cef7b3545b14b890ec8e45cd
Edwin Munevar <emuneva@gmail.com>
2020-08-07 17:29:16 -0500

    Funcion suma

--- testGit.cpp
+++ testGit.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
+int add(const int& a, const int& b)
+{
+ return a + b;
+} 
+
+
 int main()
 {
   std::cout << "Hello W" << std::endl;
+  std::cout << add(5, 6) << std:: endl;
   return 0;
  
 }
